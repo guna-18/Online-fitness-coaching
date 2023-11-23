28b86673429c73c5f8a9ab52af21391f046a6ed3
guna-18 <a.gunasekhar007@gmail.com>
2023-11-22 22:26:52 -0600

    Created project structure

---
+++ fitnesscoachingui/src/pages/CoachHomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from'react-dom/client';
+import Button from '@mui/material/Button';
+
+export default  function CoachHomePage(){
+return(<Button variant="contained">Hello world</Button>);
+}
+
