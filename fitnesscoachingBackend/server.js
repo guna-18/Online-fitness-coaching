0f069848007dd388b564913816c0ecf2006fa62c
guna-18 <a.gunasekhar007@gmail.com>
2023-12-09 09:29:06 -0600

    added cors to backend

--- fitnesscoachingBackend/server.js
+++ fitnesscoachingBackend/server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 app.use(express.static('public'));
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cors());
 
 const coachRoutes = require('./routes/coachRoutes');
 app.use('/coach', coachRoutes) ;
