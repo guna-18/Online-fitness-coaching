bbfd4504da39466da5c7dd7e8b2b645ccae5b529
guna-18 <a.gunasekhar007@gmail.com>
2023-12-09 22:42:39 -0600

    Added login and registration page validation and redirects

--- fitnesscoachingui/src/App.js
+++ fitnesscoachingui/src/App.js
@@ -7,23 +7,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from "./components/admin/Admin";
 import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
+import {useState} from 'react';
 
 
 function App() {
+  const [userID,setUserId] = useState(null);
+  const [userType,setUserType] = useState(null);
+  const changeUserId = (id)=>{
+    setUserId(id);
+  }
+  const changeUserType = (type) => {
+    setUserType(type);
+  } 
   return (
 <BrowserRouter>
 
-<CoachNavBar/>
+<CoachNavBar userType={userType}/>
       <Routes>
-        <Route path="/" element={<CoachHomePage/>}/>
-        <Route path="/clients" element={<CoachClients/>}/>
+      <Route path="/" element={<LoginPage changeUserId={changeUserId} changeUserType={changeUserType}/>}/>
+        <Route path="/coachHomepage" element={<CoachHomePage/>}/>
+        <Route path="/clients" element={<CoachClients userId={userID}/>}/>
         <Route path="/admin" element={<Admin/>}/>
         <Route
   path="/clients/:clientId"
   element={<CoachAddExercises />}
 />
 <Route path="/register" element={<RegistrationPage/>}/>
-<Route path="/login" element={<LoginPage/>}/>
+<Route path="/login" element={<LoginPage changeUserId={changeUserId} changeUserType={changeUserType}/>}/>
        
       </Routes>
 </BrowserRouter>
